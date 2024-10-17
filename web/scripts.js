c81ea18746bdc9e529050939653deaba662f0335
Sravan7721 <160935571+Sravan7721@users.noreply.github.com>
2024-10-17 16:20:55 +0530

    Add files via upload

---
+++ web/scripts.js
@@ -0,0 +1,14 @@
+a, button {
+    padding: 10px;
+    font-size: 16px;
+    display: inline-block;
+    margin: 10px 0;
+}
+
+/* Increase padding for mobile touch targets */
+@media (max-width: 600px) {
+    a, button {
+        padding: 15px;
+        font-size: 18px;
+    }
+}
